50417cc1e4faf767e695327d404ba92ad87d0909
Archan Ray <talk2archan@gmail.com>
2026-06-14 23:47:02 -0400

    update score

--- assets/data/worldcup26-data.js
+++ assets/data/worldcup26-data.js
@@ -241,7 +241,7 @@ window.WORLD_CUP_STATE = {
         "id": "F2",
         "a": "SWE",
         "b": "TUN",
-        "ga": 3,
+        "ga": 4,
         "gb": 1
       },
       {
@@ -712,7 +712,7 @@ window.WORLD_CUP_STATE = {
   },
   "meta": {
     "source": "manual data file",
-    "updatedAt": "2026-06-15T03:43:51.238Z",
+    "updatedAt": "2026-06-15T03:46:56.044Z",
     "matchesSeen": 0
   }
 };
