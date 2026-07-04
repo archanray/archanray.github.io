c42932d5ba702122dbbeddc003c0563ff69cf8ca
Archan Ray <talk2archan@gmail.com>
2026-07-04 14:39:28 -0400

    Update match scores for World Cup 2026 data

--- assets/data/worldcup26-data.js
+++ assets/data/worldcup26-data.js
@@ -638,8 +638,8 @@ window.WORLD_CUP_STATE = {
       "winner": "EGY"
     },
     "89": {
-      "a": null,
-      "b": null,
+      "a": 0,
+      "b": 1,
       "winner": null
     },
     "90": {
