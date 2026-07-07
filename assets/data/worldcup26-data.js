c3cd00a24ee3473a6a6bbab875039e106c5d06ae
Archan Ray <talk2archan@gmail.com>
2026-07-07 09:39:09 -0400

    update

--- assets/data/worldcup26-data.js
+++ assets/data/worldcup26-data.js
@@ -658,13 +658,13 @@ window.WORLD_CUP_STATE = {
       "winner": null
     },
     "93": {
-      "a": null,
-      "b": null,
+      "a": 0,
+      "b": 1,
       "winner": null
     },
     "94": {
-      "a": null,
-      "b": null,
+      "a": 1,
+      "b": 4,
       "winner": null
     },
     "95": {
